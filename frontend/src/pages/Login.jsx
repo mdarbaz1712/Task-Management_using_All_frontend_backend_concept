5b31f4a115cce90c9a1b846073e29556d0da13b6
Md Arbaz <143584027+mdarbaz1712@users.noreply.github.com>
2025-09-14 15:38:04 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from "axios"
 import { authActions } from '../store/auth'
-import { backend_url } from '../../store/store';
+import { backend_url } from '../store/store';
 import { useDispatch ,useSelector} from 'react-redux'
 
 const Login = () => {
